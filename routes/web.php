979c49f2e13305dd933d19f996584e6799da0eac
Darkedoviz <146472779+Darkedoviz@users.noreply.github.com>
2025-04-12 07:05:34 +0500

    Добавлен Первый контроллер с DTO и 3 маршрута

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/info/server', 'App\Http\Controllers\InfoController@serverInfo');
+Route::get('/info/client', 'App\Http\Controllers\InfoController@clientInfo');
+Route::get('/info/database', 'App\Http\Controllers\InfoController@databaseInfo');
+
+
